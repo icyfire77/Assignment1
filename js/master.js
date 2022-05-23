7e5ca1bf46cbbcfb30b8787678310015698e3f1d
icyfire77 <ivanfeng1@hotmail.com>
2022-05-23 16:10:31 -0700

    dynamic text added

--- js/master.js
+++ js/master.js
@@ -1,3 +1,36 @@
+// GLOBAL VARIABLES:
+let dynamicSize = 20; //refactor later if possible
+
+// https://stackoverflow.com/questions/2170923/whats-the-easiest-way-to-call-a-function-every-5-seconds-in-jquery
+function dynamicHeader() {
+
+  /*
+  const header = document.querySelector('.header');
+  const fontSize = getComputedStyle(header).getPropertyValue("font-size");
+  console.log(fontSize);
+
+  let dynamicSize = parseInt(fontSize.substring(0, fontSize.length-2));
+  */
+
+  if (dynamicSize >= 5) {
+    growing = false;
+    dynamicSize = 4.9;
+  } else if (dynamicSize <= 3) {
+    growing = true;
+    dynamicSize = 3.1;
+  }
+
+  if (growing === true) {
+    dynamicSize += 0.1;
+  } else {
+    dynamicSize -= 0.1;
+  }
+
+  document.getElementsByClassName("header")[0].style.fontSize=dynamicSize + "vh";
+  console.log(dynamicSize);
+}
+
+let dynamicInterval = setInterval(dynamicHeader, 40);
 
 function saveRecipe() {
 
@@ -7,7 +40,7 @@ function clearInput() {
 
 }
 
-function getWidth() {
+function getWidth() { // currently not used, but may come in handy later
   let width = window.innerWidth;
   return width;
 }
